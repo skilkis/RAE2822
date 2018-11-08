86780811039b74d63e53e6f065a42a2a7a87144b
skilkis <sankilkis@msn.com>
2018-11-08 19:32:45 +0100

    Changed blocking points

--- geometry/blocking.py
+++ geometry/blocking.py
@@ -20,18 +20,18 @@ from directories import DIRS
 from geometry.definitions import Point, Vector
 import scipy.optimize as so
 from matplotlib import pyplot as plt
-from math import atan, degrees
+from math import atan, degrees, tan
 
 
 class Blocking(object):
 
-    def __init__(self, domain_in=None, le_angle=60, refine_distance=0.3):
+    def __init__(self, domain_in=None, le_angle=35, refine_distance=0.3, x_value=0.01):
         self.domain_in = domain_in
         self.airfoil_in = self.domain_in.airfoil_in
         self.crv_top = self.airfoil_in.curve['top']
         self.crv_bot = self.airfoil_in.curve['bot']
         self.le_angle = le_angle
-        self.le_zone = self.find_le_zone(self.le_angle)
+        self.le_zone = self.find_le_zone(le_angle)
         self.refine_distance = refine_distance
 
     @property
@@ -54,6 +54,20 @@ class Blocking(object):
 
         return {'top': u_top, 'bot': u_bot}
 
+    # def find_le_zone(self, x_value):
+    #     top, bot = self.crv_top, self.crv_bot
+    #
+    #     def objective(u, *args):
+    #         """ Returns the error between the requested x_value and the current x value at u """
+    #         crv, target_x = args
+    #         point = crv.point_at_parameter(u)
+    #         return point.x - target_x
+    #
+    #     u_top = so.brentq(objective, 0., 0.5, args=(top, x_value))
+    #     u_bot = so.brentq(objective, 0., 0.5, args=(bot, x_value))
+    #
+    #     return {'top': u_top, 'bot': u_bot}
+
     @property
     def pt_1(self):
         u_top, _ = self.airfoil_in.get_maxima()
@@ -66,69 +80,61 @@ class Blocking(object):
 
     @property
     def pt_2(self):
-        return self.crv_top.point_at_parameter(0.7)
-
-    @property
-    def pt_2_top(self):
-        return self.pt_2.translate(self.crv_top.normal(0.7) * self.refine_distance)
-
-    @property
-    def pt_3(self):
         return self.airfoil_in.trailing_edge
 
     @property
-    def pt_3_top(self):
-        return self.pt_3.translate(self.crv_top.normal(1.0)*self.refine_distance)
+    def pt_2_top(self):
+        return self.pt_2.translate(self.crv_top.normal(1.0)*self.refine_distance)
 
     @property
-    def pt_3_bot(self):
-        return self.pt_3.translate(self.crv_bot.normal(1.0) * - self.refine_distance)
+    def pt_2_bot(self):
+        return self.pt_2.translate(self.crv_bot.normal(1.0) * - self.refine_distance)
+
+    # @property
+    # def pt_4(self):
+    #     return self.pt_2.translate(self.crv_top.tangent(1.0) * 0.1)
+    #
+    # @property
+    # def pt_4_bot(self):
+    #     return self.pt_4.translate(self.crv_bot.normal(1.0) * -self.refine_distance)
+    #
+    # @property
+    # def pt_4_top(self):
+    #     return self.pt_4.translate(self.crv_top.normal(1.0) * self.refine_distance)
+    #
+    # @property
+    # def pt_5(self):
+    #     return self.crv_bot.point_at_parameter(0.7)
+    #
+    # @property
+    # def pt_5_bot(self):
+    #     return self.pt_5.translate(self.crv_bot.normal(0.7) * -self.refine_distance)
 
     @property
-    def pt_4(self):
-        return self.pt_3.translate(self.crv_top.tangent(1.0) * 0.3)
-
-    @property
-    def pt_4_bot(self):
-        return self.pt_4.translate(self.crv_bot.normal(1.0) * -self.refine_distance)
-
-    @property
-    def pt_4_top(self):
-        return self.pt_4.translate(self.crv_top.normal(1.0) * self.refine_distance)
-
-    @property
-    def pt_5(self):
-        return self.crv_bot.point_at_parameter(0.7)
-
-    @property
-    def pt_5_bot(self):
-        return self.pt_5.translate(self.crv_bot.normal(0.7) * -self.refine_distance)
-
-    @property
-    def pt_6(self):
+    def pt_3(self):
         _, u_bot = self.airfoil_in.get_maxima()
         return self.crv_bot.point_at_parameter(u_bot)
 
     @property
-    def pt_6_bot(self):
+    def pt_3_bot(self):
         _, u_bot = self.airfoil_in.get_maxima()
-        return self.pt_6.translate(self.crv_bot.normal(u_bot) * -self.refine_distance)
+        return self.pt_3.translate(self.crv_bot.normal(u_bot) * -self.refine_distance)
 
     @property
-    def pt_7(self):
+    def pt_4(self):
         return self.crv_bot.point_at_parameter(self.le_zone['bot'])
 
     @property
-    def pt_7_bot(self):
-        return self.pt_7.translate(self.crv_bot.normal(self.le_zone['bot']) * -self.refine_distance)
+    def pt_4_bot(self):
+        return self.pt_4.translate(self.crv_bot.normal(self.le_zone['bot']) * -self.refine_distance)
 
     @property
-    def pt_8(self):
+    def pt_5(self):
         return self.crv_top.point_at_parameter(self.le_zone['top'])
 
     @property
-    def pt_8_top(self):
-        return self.pt_8.translate(self.crv_top.normal(self.le_zone['top']) * self.refine_distance)
+    def pt_5_top(self):
+        return self.pt_5.translate(self.crv_top.normal(self.le_zone['top']) * self.refine_distance)
 
     @property
     def pt_le_proj(self):
@@ -136,7 +142,7 @@ class Blocking(object):
 
     def plot(self):
         fig = plt.figure('{}Airfoil'.format(self.airfoil_in.__name__))
-        plt.style.use('tudelft')
+        # plt.style.use('tudelft')
         ord_dict = self.airfoil_in.get_ordinates()
         top_x, top_y, bot_x, bot_y = (ord_dict['top'] + ord_dict['bot'])
         plt.plot(top_x, top_y, label='Top Surface')
@@ -147,38 +153,67 @@ class Blocking(object):
 
         plt.scatter(self.pt_2.x, self.pt_2.y)
         plt.scatter(self.pt_2_top.x, self.pt_2_top.y)
+        plt.scatter(self.pt_2_bot.x, self.pt_2_bot.y)
 
         plt.scatter(self.pt_3.x, self.pt_3.y)
-        plt.scatter(self.pt_3_top.x, self.pt_3_top.y)
-        plt.scatter(self.project(self.pt_3_top, 'top').x, self.project(self.pt_3_top, 'top').y)
+        plt.scatter(self.pt_3.x, self.pt_3.y)
         plt.scatter(self.pt_3_bot.x, self.pt_3_bot.y)
-        plt.scatter(self.project(self.pt_3_bot, 'bot').x, self.project(self.pt_3_bot, 'bot').y)
 
         plt.scatter(self.pt_4.x, self.pt_4.y)
-        plt.scatter(self.project(self.pt_4, 'wake').x, self.project(self.pt_4, 'wake').y)
-        plt.scatter(self.pt_4_top.x, self.pt_4_top.y)
-        plt.scatter(self.project(self.pt_4_top, 'top').x, self.project(self.pt_4_top, 'top').y)
-        plt.scatter(self.project(self.pt_4_top, 'wake').x, self.project(self.pt_4_top, 'wake').y)
         plt.scatter(self.pt_4_bot.x, self.pt_4_bot.y)
-        plt.scatter(self.project(self.pt_4_bot, 'bot').x, self.project(self.pt_4_bot, 'bot').y)
-        plt.scatter(self.project(self.pt_4_bot, 'wake').x, self.project(self.pt_4_bot, 'wake').y)
 
         plt.scatter(self.pt_5.x, self.pt_5.y)
-        plt.scatter(self.pt_5_bot.x, self.pt_5_bot.y)
-        plt.scatter(self.project(self.pt_5_bot, 'bot').x, self.project(self.pt_5_bot, 'bot').y)
-
-        plt.scatter(self.pt_6.x, self.pt_6.y)
-        plt.scatter(self.pt_6_bot.x, self.pt_6_bot.y)
-
-        plt.scatter(self.pt_7.x, self.pt_7.y)
-        plt.scatter(self.pt_7_bot.x, self.pt_7_bot.y)
+        plt.scatter(self.pt_5_top.x, self.pt_5_top.y)
 
         plt.scatter(self.airfoil_in.leading_edge.x, self.airfoil_in.leading_edge.y)
-
         plt.scatter(self.pt_le_proj.x, self.pt_le_proj.y)
 
-        plt.scatter(self.pt_8.x, self.pt_8.y)
-        plt.scatter(self.pt_8_top.x, self.pt_8_top.y)
+        # Calculating Pt_2 Domain Projection
+        pt_2 = self.pt_2_top
+        y = self.domain_in.pt_2.y - self.pt_2_top.y
+        print(y)
+        v_norm = self.crv_top.normal(1.0)
+        print(v_norm)
+        x = y / (v_norm.y / v_norm.x)
+        print(degrees(v_norm.y / v_norm.x))
+
+        plt.scatter(self.pt_2_top.x + x, self.domain_in.pt_2.y)
+
+
+
+
+
+        # plt.scatter(self.pt_3.x, self.pt_3.y)
+        # plt.scatter(self.pt_3_top.x, self.pt_3_top.y)
+        # plt.scatter(self.project(self.pt_3_top, 'top').x, self.project(self.pt_3_top, 'top').y)
+        # plt.scatter(self.pt_3_bot.x, self.pt_3_bot.y)
+        # plt.scatter(self.project(self.pt_3_bot, 'bot').x, self.project(self.pt_3_bot, 'bot').y)
+        #
+        # plt.scatter(self.pt_4.x, self.pt_4.y)
+        # plt.scatter(self.project(self.pt_4, 'wake').x, self.project(self.pt_4, 'wake').y)
+        # plt.scatter(self.pt_4_top.x, self.pt_4_top.y)
+        # plt.scatter(self.project(self.pt_4_top, 'top').x, self.project(self.pt_4_top, 'top').y)
+        # plt.scatter(self.project(self.pt_4_top, 'wake').x, self.project(self.pt_4_top, 'wake').y)
+        # plt.scatter(self.pt_4_bot.x, self.pt_4_bot.y)
+        # plt.scatter(self.project(self.pt_4_bot, 'bot').x, self.project(self.pt_4_bot, 'bot').y)
+        # plt.scatter(self.project(self.pt_4_bot, 'wake').x, self.project(self.pt_4_bot, 'wake').y)
+        #
+        # plt.scatter(self.pt_5.x, self.pt_5.y)
+        # plt.scatter(self.pt_5_bot.x, self.pt_5_bot.y)
+        # plt.scatter(self.project(self.pt_5_bot, 'bot').x, self.project(self.pt_5_bot, 'bot').y)
+        #
+        # plt.scatter(self.pt_6.x, self.pt_6.y)
+        # plt.scatter(self.pt_6_bot.x, self.pt_6_bot.y)
+        #
+        # plt.scatter(self.pt_7.x, self.pt_7.y)
+        # plt.scatter(self.pt_7_bot.x, self.pt_7_bot.y)
+        #
+        # plt.scatter(self.airfoil_in.leading_edge.x, self.airfoil_in.leading_edge.y)
+        #
+        # plt.scatter(self.pt_le_proj.x, self.pt_le_proj.y)
+        #
+        # plt.scatter(self.pt_8.x, self.pt_8.y)
+        # plt.scatter(self.pt_8_top.x, self.pt_8_top.y)
         # plt.scatter(self.pt_4_top_proj.x, self.pt_4_top_proj.y)
 
         # top_pnt = top_pnt.translate(crv_top.normal(u_top) * 0.3)
@@ -270,45 +305,56 @@ class Blocking(object):
         with open(filename, 'w') as output:
             output.write('# {} AIRFOIL BLOCKING POINTS\n'.format(name))
 
-            # Airfoil Trailing Edge Mid
-            point_format(output, self.pt_3)
-            point_format(output, self.pt_4)
-            point_format(output, self.project(self.pt_4, 'wake'))
+            # # Airfoil Trailing Edge Mid
+            # point_format(output, self.pt_2)
+            # point_format(output, self.pt_4)
+            # point_format(output, self.project(self.pt_4, 'wake'))
+            #
+            # # Airfoil Trailing Edge Top
+            # point_format(output, self.pt_3_top)
+            # point_format(output, self.project(self.pt_3_top, 'top'))
+            # point_format(output, self.pt_4_top)
+            # point_format(output, self.project(self.pt_4_top, 'wake'))
+            # point_format(output, self.project(self.pt_4_top, 'top'))
+            #
+            # # Airfoil Trailing Edge Bottom
+            # point_format(output, self.pt_3_bot)
+            # point_format(output, self.project(self.pt_3_bot, 'bot'))
+            # point_format(output, self.pt_4_bot)
+            # point_format(output, self.project(self.pt_4_bot, 'wake'))
+            # point_format(output, self.project(self.pt_4_top, 'top'))
 
-            # Airfoil Trailing Edge Top
-            point_format(output, self.pt_3_top)
-            point_format(output, self.project(self.pt_3_top, 'top'))
-            point_format(output, self.pt_4_top)
-            point_format(output, self.project(self.pt_4_top, 'wake'))
-            point_format(output, self.project(self.pt_4_top, 'top'))
+            # Airfoil Trailing Edge
+            point_format(output, self.project(self.pt_2, 'wake'))
+            point_format(output, self.project(self.pt_2_top, 'wake'))
 
-            # Airfoil Trailing Edge Bottom
-            point_format(output, self.pt_3_bot)
-            point_format(output, self.project(self.pt_3_bot, 'bot'))
-            point_format(output, self.pt_4_bot)
-            point_format(output, self.project(self.pt_4_bot, 'wake'))
-            point_format(output, self.project(self.pt_4_top, 'top'))
+            # Airfoil Trailing Edge Upper Top Projection
+            pt_2 = self.pt_2_top
+            y = self.domain_in.pt_2.y - self.pt_2_top.y
+            v_norm = self.crv_top.normal(1.0)
+            x = y / (v_norm.y / v_norm.x)
+            point_format(output, Point(self.pt_2.x + x, self.domain_in.pt_2.y, 0))
+            print(degrees(v_norm.y / v_norm.x))
+
+            point_format(output, self.project(self.pt_2_bot, 'wake'))
+            point_format(output, self.project(self.pt_2_bot, 'bot'))
 
             # Airfoil Surface Points
             point_format(output, self.pt_1)
             point_format(output, self.pt_2)
             point_format(output, self.pt_3)
+            point_format(output, self.pt_4)
             point_format(output, self.pt_5)
-            point_format(output, self.pt_6)
-            point_format(output, self.pt_7)
-            point_format(output, self.pt_8)
 
             # Airfoil Refinment Spline
             output.write(' {:d} 0\n'.format(9))
+            point_format(output, self.pt_2_bot)
             point_format(output, self.pt_3_bot)
-            point_format(output, self.pt_5_bot)
-            point_format(output, self.pt_6_bot)
-            point_format(output, self.pt_7_bot)
+            point_format(output, self.pt_4_bot)
             point_format(output, self.pt_le_proj)
-            point_format(output, self.pt_8_top)
+            point_format(output, self.pt_5_top)
             point_format(output, self.pt_1_top)
             point_format(output, self.pt_2_top)
-            point_format(output, self.pt_3_top)
 
             # # Segment 2-3
             # output.write(' {:d} 0\n'.format(2))
@@ -331,7 +377,7 @@ if __name__ == '__main__':
     from geometry.airfoil import Airfoil
     from geometry.domain import Domain
     obj = Blocking(Domain(Airfoil(angle=2.31), upstream=13., top=13., bottom=13., wake=20.))
-    obj.plot()
+    # obj.plot()
     obj.write_dat()
     #
     # x = Vector(1, 0, 0)
