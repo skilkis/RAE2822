e21a5eda8337682204f999116cfc58fe5bd892ce
skilkis <sankilkis@msn.com>
2018-11-10 14:57:47 +0100

    Added way to compute h for y+ = 0.5

--- analysis/yplus.py
+++ analysis/yplus.py
@@ -1,6 +1,7 @@
 from directories import DIRS
 import os
 from math import sqrt
+from inflow import rho, mu
 
 with open(os.path.join(DIRS['DATA_DIR'], 'shear', 'coarse_wallshear.csv')) as f:
     lines = f.readlines()[5:]
@@ -8,10 +9,14 @@ with open(os.path.join(DIRS['DATA_DIR'], 'shear', 'coarse_wallshear.csv')) as f:
              for line in lines]
     average_shear = sum([i[1] for i in lines])/len(lines)
 
-
-
-from inflow import rho, mu
 u_tau = sqrt(average_shear/rho)
 
 h = mu / u_tau
-print('First Cell Height <= {} for y+ <1'.format(h))
+
+
+def first_cell_height(y_plus=0.5):
+    """ Computes Cell Height from a desired y+ value """
+    return y_plus * mu / u_tau
+
+
+print('First Cell Height <= {} for y+ <1'.format(first_cell_height(0.5)))
